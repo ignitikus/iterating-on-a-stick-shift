9fa88d29ebd0b029877b4ed2744f185526ecc8b5
NikolayK <nikolay.kim@codeimmersive.com>
2019-12-02 13:18:20 -0500

    adds solution

--- main.js
+++ main.js
@@ -1,14 +1,38 @@
-const makeDino = function() {
+const makeDino = function(species, period, carnivore, extinct=false) {
+  return {
+    species,
+    period,
+    carnivore,
+    extinct,
+  }
 }
 
-const makeSingular = function() {
+const makeSingular = function(dino) {
+  return newDino = {
+    species: dino.species.slice(dino.species.length-2) === 'us'? dino.species.slice(0, dino.species.length-2):dino.species
+    ,
+    period: dino.period,
+    carnivore: dino.carnivore,
+    extinct: dino.extinct,
+  }
 }
 
-const truncateSpecies = function() {
+const truncateSpecies = function(dino) {
+  return newDino = {
+    species: dino.species.length>=10 ? dino.species.slice(0, 7)+'...' : dino.species,
+    period: dino.period,
+    carnivore: dino.carnivore,
+    extinct: dino.extinct,
+  }
 }
 
-const makeExtinct = function() {
-
+const makeExtinct = function(dino) {
+  return newDino = {
+    species: dino.species,
+    period: dino.period,
+    carnivore: dino.carnivore,
+    extinct: true,
+  }
 }
 
 module.exports = {
